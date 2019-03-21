8a4eccb2291558d941a1f2abc0d9372135b136d9
agustin <carlos.agustin.diaz1998@gmail.com>
2019-03-21 19:39:15 -0300

    7ma clase de Vue CLI con bootstrap-vue form(number, text)

--- BootstrapVue/src/router.js
+++ BootstrapVue/src/router.js
@@ -34,6 +34,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "card" */ './views/Card.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "form" */ './views/Form.vue')
     }
   ]
 })
---
+++ BootstrapVue/src/views/Form.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="mt-5 container">
+        <h1>Formularios</h1>
+        <form>
+            <b-row>
+                <b-col cols="12" md="6">
+                    <b-form-input type="number" v-model="numero"></b-form-input>
+                </b-col>
+                <b-col cols="12" md="6">
+                    <b-form-input type="texto" v-model="texto" placeholder="Texto" 
+                        :state="comprobar"
+                    ></b-form-input>
+                    <small>Minimo 3 caracteres</small>
+                </b-col>
+            </b-row>
+        </form>
+        <hr>
+        <div class="mt-2">
+            <p>numero: {{numero}}</p>
+        </div>
+        <div class="mt-2">
+            <p>Texto: {{texto}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Form',
+    data(){
+        return{
+            texto:'',
+            numero: 0
+        }
+    },
+    computed: {
+        comprobar(){
+            return  this.texto.length > 2 ? true : false
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
